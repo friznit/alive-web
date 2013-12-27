98f3e276a4d0ced117f7f22900cb5d4d82b794eb
Tupolov <tupolov73@gmail.com>
2013-12-27 18:35:09 +0000

    [clans] Added parent field for groups so that we can start to form an ORBAT/hierarchy

---
+++ app/database/migrations/2013_12_27_002040_add_parent_field_to_clans.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddParentFieldToClans extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('clans', function(Blueprint $table)
+        {
+            $table->string('parent', 16)->nullable();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('clans', function(Blueprint $table)
+		{
+            $table->dropColumn('parent');
+		});
+	}
+
+}
