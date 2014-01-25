030508a251d288d8e67064b3ac9cbe01345c618e
ARJay <arjaydev@gmail.com>
2014-01-25 11:52:56 +1100

    added admin index page to de-clutter the nav in the public warroom

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,12 @@ if(Config::get('app.debug')){
 Route::controller('api', 'APIController');
 Route::controller('user', 'UserController');
 Route::controller('war-room', 'WarRoomController');
+
+Route::get('/admin', function() {
+    $data = get_default_data();
+    return View::make('admin/home/index')->with($data);;
+});
+
 Route::controller('admin/ao', 'AdminAOController');
 Route::controller('admin/application', 'AdminApplicationController');
 Route::controller('admin/clan', 'AdminClanController');
--- app/views/warroom/partials/_nav.blade.php
+++ app/views/warroom/partials/_nav.blade.php
@@ -32,11 +32,16 @@
                 <li {{ (Request::is('admin/user/show/*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/user/show/') }}/{{ Sentry::getUser()->getId() }}">Profile</a></li>
  
                 @if ($auth['isAdmin'])
+                <li><a href="{{ URL::to('admin') }}">Admin</a></li>
+
+                <!--
                 <li {{ (Request::is('admin/ao*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/ao') }}">AOs</a></li>
                 <li {{ (Request::is('admin/clan*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/clan') }}">Groups</a></li>
                 <li {{ (Request::is('admin/server*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/server') }}">Servers</a></li>
                 <li {{ (Request::is('admin/user*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/user') }}">Users</a></li>
                 <li {{ (Request::is('admin/group*') ? 'class="active"' : '') }}><a href="{{ URL::to('admin/group') }}">User Groups</a></li>
+                -->
+
                 @endif
 
                 <li id="logout"><a href="{{ URL::to('user/logout') }}">Logout</a></li>
