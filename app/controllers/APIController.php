972498aa7807aa0836a0e1912e1e2bb52036599c
ARJay <arjaydev@gmail.com>
2014-03-20 14:09:25 +1100

    put testing calls back in APIController

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -410,8 +410,6 @@ class APIController extends BaseController {
         $result = $this->getTotals();
         TempoDebug::stopProfile($profiler);
 
-        /*
-
         $profiler = TempoDebug::startProfile();
         $result = $this->getActiveunitcount();
         TempoDebug::stopProfile($profiler);
@@ -503,8 +501,6 @@ class APIController extends BaseController {
         $profiler = TempoDebug::startProfile();
         $result = $this->getOpsbreakdown();
         TempoDebug::stopProfile($profiler);
-
-        */
 
     }
 }
