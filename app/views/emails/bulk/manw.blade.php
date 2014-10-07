2bb19429146d4751a03f24cb7d85b60b0812e8fb
Tupolov <tupolov73@gmail.com>
2014-10-07 12:47:05 +0100

    [email] MANW update email

--- app/views/emails/bulk/manw.blade.php
+++ app/views/emails/bulk/manw.blade.php
@@ -4,11 +4,23 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-		<h2>Please support ALiVE in the Make Arma Not War competition</h2>
-
-		<p>ALiVE has entered the MANW competition if you could spare some time, please lend us your support!</p>
-		<p><a href="http://makearmanotwar.com/entry/0MI2rqQ5aQ#.VDPBbvmSwnJ">Support ALiVE</a></p>
-		<p>Thank you, <br />
-			~The ALiVE dev team</p>
+		<h2 align="center"><img src="http://alivemod.com/img/home_logo.png" alt="ALiVE" width="505" height="175" longdesc="http://alivemod.com"></h2>
+		<h2 align="center"><a href="http://makearmanotwar.com/entry/0MI2rqQ5aQ#.VDPBbvmSwnJ">Support ALiVE in the Make Arma Not War Contest</a></h2>
+		<h2>Thank you!</h2>
+		<p>Our beta testing is coming to an end and after 6 months, 5000 players and over 1,000,000 in game events logged to ALiVE, we wanted to thank you!</p>
+		<p>We sincerely hope you have enjoyed playing the ALiVE mod, we look forward further versions soon, but we have one final favour to ask you!		</p>
+		<p><strong>ALiVE has entered the MANW competition if you could spare some time, please lend us your support! </strong></p>
+		<ol>
+		  <li>Simply click on the following link. It will take you to the Arma 3 Make War Not Arma contest entry for ALiVE.</li>
+		  <li><a href="http://makearmanotwar.com/entry/0MI2rqQ5aQ#.VDPBbvmSwnJ">Support ALiVE</a></li>
+		  <li>You can simply add your support by clicking on the yellow box with the + sign. You may need to login (either via Facebook, Twitter, Steam, BIS Profile or create a login).</li>
+	</ol>
+		<h2>ALiVE 0.8 released! </h2>
+        <p>If you didn't know already, we recently released a new version of ALiVE. We highly recommend you update as soon as possible. More information can be found here on the <a href="http://forums.bistudio.com/showthread.php?169350-ALiVE-Advanced-Light-Infantry-Virtual-Environment&p=2785229&viewfull=1#post2785229" target="new">Bohemia Interactive Forum</a> or at our website <a href="http://alivemod.com        ">http://alivemod.com </a></p>
+        <h2>New Trailer</h2>
+        <p>We've launched a new trailer and also a new set of <a href="http://alivemod.com/#Editors">tutorials</a></p>
+    <embed width="420" height="500" src="http://www.youtube.com/v/lsztsBqNbFc">      
+        <p>Thank you, <br />
+		~The ALiVE dev team</p>
 	</body>
 </html>
