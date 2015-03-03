b87a5b442226345ff8e4a921e117d8725f5afb49
Tupolov <tupolov73@gmail.com>
2015-03-03 21:07:34 +0000

    [war room] Added notes for server performance monitoring

--- app/views/warroom/charts/serverperf.blade.php
+++ app/views/warroom/charts/serverperf.blade.php
@@ -143,3 +143,116 @@ $(function() {
 </script>
 
 <div id="container"><div class="alert alert-info" style="text-align:center">Loading...</div></div>
+
+<div class="container">
+        <div class="row">
+ 		    <div class="col-md-9">
+                <div class="panel panel-light">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">About ALiVE Performance Monitoring</h3>
+                    </div>
+
+                    {{ Form::token() }}
+
+                    <div class="panel-body">
+
+                        <div class="strip">
+                            <p><b>NOTE: Click on the chart keys to turn individual graphs on or off. Roll over graphs with mouse to see specific data points</b></p>
+                        </div>
+
+                        <table class="table">
+                             <tr>
+                                <td width="80">Players</td>
+                                <td>Number of players connected to the server</td>
+                            </tr>
+                            <tr>
+                                <td>FPS</td>
+                                <td>Frames per second on the server as recorded by diag_FPS. Indicates general load on server.</td>
+                            </tr>
+                            <tr>
+                                <td>FPSMIN</td>
+                                <td>Lowest recorded Frames per second on the server as recorded by diag_FPSMIN over the last 16 frames</td>
+                            </tr>
+                            <tr>
+                                <td>CPS</td>
+                                <td>Cycles per second (FSM) on average during the default interval (3 minutes). Low values are likely to affect AI.</td>
+                            </tr>
+                            <tr>
+                                <td>Objects</td>
+                                <td>Total number of objects in game.</td>
+                            </tr>
+                            <tr>
+                                <td>Agents</td>
+                                <td>Total number of AI in game.</td>
+                            </tr>
+                            <tr>
+                                <td>Vehicles</td>
+                                <td>Total number of vehicles in game.</td>
+                            </tr>                                                                                    
+                            <tr>
+                                <td>Entities</td>
+                                <td>Total number of dead or alive entities in game.</td>
+                            </tr> 
+                            <tr>
+                                <td>AllDead</td>
+                                <td>Total number of dead things.</td>
+                            </tr>
+                             <tr>
+                                <td>Triggers</td>
+                                <td>Total number of triggers in game.</td>
+                            </tr>                           
+                             <tr>
+                                <td>REMAI</td>
+                                <td>Total number of remote AI, for example that are local to a client and not the server.</td>
+                            </tr>   
+                              <tr>
+                                <td>LOCAI</td>
+                                <td>Total number of AI that are local to the server.</td>
+                            </tr>  
+                              <tr>
+                                <td>ActiveProf</td>
+                                <td>Total number of profiles that are active and spawned.</td>
+                            </tr>
+                              <tr>
+                                <td>InactiveProf</td>
+                                <td>Total number of profiles that are cached/virtualized</td>
+                            </tr>
+                               <tr>
+                                <td>UnitsProf</td>
+                                <td>Total number of units profiled</td>
+                            </tr>          
+                              <tr>
+                                <td>VehProf</td>
+                                <td>Total number of vehicles that are profiled</td>
+                            </tr>    
+                            <tr>
+                                <td>Mem</td>
+                                <td>Amount of memory in MB the current arma3server.exe process is using</td>
+                            </tr> 
+                            <tr>
+                                <td>CPU</td>
+                                <td>Percentage of CPU use for the arma3server.exe process (taking into account total cores)</td>
+                            </tr>                             
+                            <tr>
+                                <td>Threads</td>
+                                <td>Number of threads currently associated with the arma3server.exe process</td>
+                            </tr>                              
+                            <tr>
+                                <td>IORead</td>
+                                <td>Amount of read IO in MB since the last sample (network and disk)</td>
+                            </tr>                            
+                            <tr>
+                                <td>IOWrite</td>
+                                <td>Amount of write IO in MB since the last sample (network and disk)</td>
+                            </tr>                            
+                            <tr>
+                                <td>DiskQueue</td>
+                                <td>Equivalent to Avg. Disk Queue Length which is equal to the (Disk Transfers per second)*( Disk seconds to Transfer)</td>
+                            </tr>                                                                                                                                                                                                      
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+      </div>
+</div>            
