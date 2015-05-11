d793ce468cce46ec50a7af999d10437e1b5b6cff
Tupolov <tupolov73@gmail.com>
2015-05-11 10:17:14 +0100

    [Warroom] Updated home page, removed MANW notice and added donation notice

--- app/views/warroom/layouts/home_footer.blade.php
+++ app/views/warroom/layouts/home_footer.blade.php
@@ -6,14 +6,14 @@
 
 <div id="warroom_notice">
     <div class="strip"><span class="control-right" id="warroom_notice_close"><i class="fa fa-times"></i></span><span id="warroom_livefeed_label" class="control">Notice</span></div>
+    <h2>We're running low on server funds! <a href="http://alivemod.com/#Donate">Donate now!</a></h2>
+    <p>It's easy to donate via Pay Pal or Bitcoin. Click <a href="http://alivemod.com/#Donate">here</a> to donate.</p>    
 	<h2>War Room Upgrade</h2>
-    <p>NEW! We've catalogued thousands of weapons and vehicles from popular mods. This pimps your <a href="{{ URL::to('war-room/showpersonnel') }}/{{ $auth['profile']->a3_id }}">Stats Page</a>. Check it out today!</p>        
-    <h2>MANW ALiVE!</h2>
-    <p><a href="http://makearmanotwar.com/entry/0MI2rqQ5aQ#.VCx96hawQ6U">Click here to support us</a> in the Make Arma Not War Contest</p>    
+    <p>NEW! We've catalogued thousands of weapons and vehicles from popular mods. This pimps your <a href="{{ URL::to('war-room/showpersonnel') }}/{{ $auth['profile']->a3_id }}">Statistics Page</a>. Check it out today!</p>        
     <h2>War Room forum footers</h2>
-    <p>Display your stats in forums and on websites! We have added the code you need to use to player profile pages.</p>
+    <p>Display your stats in forums and on websites! We have added the code you need to use to your <a href="{{ URL::to('admin/user/show/') }}/{{ $auth['userId'] }}">Player Profile</a> page.</p>
     <h2>General Support Requests</h2>
-    <p>For any support issues please join the skype public ALiVE group. If you can send a PM to a team member on the forums with your skype details we will add you in.</p>
+    <p>For any support issues please post on the <a href="http://alivemod.com/forum/">ALiVE Forum</a>.</p>
 
 </div>
 
