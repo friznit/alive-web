077d4e240c199c09b03ba30061b70c1f8b3a2b50
Tupolov <tupolov73@gmail.com>
2015-06-14 09:03:58 +0100

    [warroom] Update player feed to include link to operation page

--- app/views/warroom/tables/player_feed.blade.php
+++ app/views/warroom/tables/player_feed.blade.php
@@ -12,7 +12,7 @@
                             .append(row.value.Map + ' - Grid:' + row.value.KilledPos + ' - ')
                             .append(row.value.gameTime + ' local<br>')
                             .append(row.value.Killedfaction + ' ' + row.value.KilledType + '<span class="highlight"> ' + row.value.PlayerName + '</span> has been KIA')
-                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
 
                     } else {
                         if (row.value.KilledClass != "Infantry")
@@ -21,7 +21,7 @@
                                 .append(row.value.Map + ' - Grid:' + row.value.KilledPos + ' - ')
                                 .append(row.value.gameTime + ' local<br>')
                                 .append(row.value.Killedfaction + ' <span class="highlight">' + row.value.KilledType + '</span> has been destroyed')
-                                .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                                .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                         } else {
                             $('#player_feed')
                                 .append(row.value.Map + ' - Grid:' + row.value.KilledPos + ' - ')
@@ -31,7 +31,7 @@
                                 .append('<span class="highlight"> ' + row.value.KilledType)
                                 .append('</span> with an ' + row.value.Weapon)
                                 .append(' from ' + row.value.Distance + 'm')
-                                .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                                .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                         }
                     }
 
@@ -43,7 +43,7 @@
                         .append(row.value.Map + ' - ')
                         .append(row.value.gameTime + ' local<br>')
                         .append(row.value.PlayerName + ' is participating in Operation <span class="highlight2">' + row.value.Operation + '</span>.')
-                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                 }
 
                 if (row.value.Event == "PlayerFinish")
@@ -52,7 +52,7 @@
                         .append(row.value.Map + ' - ')
                         .append(row.value.gameTime + ' local<br>')
                         .append(row.value.PlayerName + ' has left Operation <span class="highlight2">' + row.value.Operation + '</span> after ' + row.value.timePlayed + ' minutes.')
-                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                 }
 
                 if (row.value.Event == "Hit" && !(row.value.PlayerHit))
@@ -61,7 +61,7 @@
                         .append(row.value.Map + ' - Grid:' + row.value.hitPos + ' - ')
                         .append(row.value.gameTime + ' local<br>')
                         .append(row.value.sourcefaction + ' ' + row.value.sourceType + ' <span class="operation">(' + row.value.PlayerName + ')</span> has scored a hit on a ' + row.value.hitfaction + ' ' + row.value.hitType + '.')
-                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                        .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                 }
 
                 if (row.value.Event == "Missile")
@@ -72,7 +72,7 @@
                             .append(row.value.Map + ' - Grid:' + row.value.targetPos + ' - ')
                             .append(row.value.gameTime + ' local<br>')
                             .append(row.value.targetFaction + ' ' + row.value.targetType + ' <span class="highlight3">(' + row.value.PlayerName + ')</span> has been engaged by a ' + row.value.sourceFaction + ' ' + row.value.sourceType + '.')
-                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                     } else {
                         $('#player_feed')
                             .append(row.value.Map + ' - Grid:' + row.value.sourcePos + ' - ')
@@ -82,7 +82,7 @@
                             .append(row.value.targetType)
                             .append(' with a ' + row.value.Weapon)
                             .append(' from ' + row.value.Distance + 'm using a ' + row.value.projectile)
-                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation">Operation ' + row.value.Operation +'</span><hr>')
+                            .append('<br>' + parseArmaDate(row.key[1]) + ' - <span class="operation"><a href={{ URL::to("war-room/showoperation?name=") }}' + encodeURIComponent(row.value.Operation) +'&map=' + encodeURIComponent(row.value.Map) +'&clan={{{$clan->tag}}}>Operation ' + row.value.Operation +'</span><hr>')
                     }
 
                 }
