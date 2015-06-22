1b5c424a71515a13b36e18db0ae74bf1092e7e42
Tupolov <tupolov73@gmail.com>
2015-06-22 14:24:51 +0100

    [home] Updated website to 0.9.9 version

--- app/views/public/home/index.blade.php
+++ app/views/public/home/index.blade.php
@@ -180,9 +180,9 @@
                         <th>Donate</th>
                     </tr>
                     <tr class="success">
-                        <td>0.9.8.1505111</td>
-                        <td>Arma 3 Stable 1.44.130654 </td>
-                        <td><a class="btn btn-primary btn-lg pull-right" href="http://alivemod.com/downloads/@ALiVE_0.9.8.1505111.7z"><i class="fa fa-download"></i> Download</a></td>
+                        <td>0.9.9.1506221</td>
+                        <td>Arma 3 Stable 1.46.131175</td>
+                        <td><a class="btn btn-primary btn-lg pull-right" href="http://alivemod.com/downloads/@ALiVE_0.9.9.1506221.7z"><i class="fa fa-download"></i> Download</a></td>
  						<td><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                                 <input type="image" class="btn btn-yellow btn-lg" name="submit" alt="Voluntary Donation" style="margin-top: -10px;">
                                 <input type="hidden" name="cmd" value="_s-xclick">
