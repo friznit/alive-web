e5435a61dc08c85fc532922a672d6a8d92393512
Tupolov <tupolov73@gmail.com>
2015-09-16 07:18:05 +0100

    [war room] Updated alive_news.php to include latest ALiVE version

--- public/public_html/alive_news.php
+++ public/public_html/alive_news.php
@@ -32,8 +32,8 @@ echo $date
 <?php
 echo "Welcome ". $player. " to ALiVE Operations on ". $map. "<br />";
 echo '<br>';
-echo 'Latest ALiVE Version:<br> 0.9.7.1504161<br />';
-echo 'Compatible with:<br> Arma 3 Stable 1.42.130340<br />';
+echo 'Latest ALiVE Version:<br> 0.9.9.1506221<br />';
+echo 'Compatible with:<br> Arma 3 Stable 1.46.131175<br />';
 echo '<br />-<a href="http://www.alivemod.com/#Download"> Download ALiVE here!</a>';
 echo '<br />-<a href="http://www.alivemod.com/war-room"> Access War Room here!</a>';
 echo '<br />-<a href="http:\\alivemod.com"> Get ALiVE updates here!</a>';
