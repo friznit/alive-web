d887a4cdef5690ce9263c5bb5850d2636bb0e8e4
Andrew Khan <andrew@tjcuk.co.uk>
2015-10-01 13:25:45 +0100

    Format War Room to PSR (2)

--- app/controllers/WarRoomController.php
+++ app/controllers/WarRoomController.php
@@ -3,7 +3,8 @@
 use Alive\CouchAPI;
 use Tempo\TempoDebug;
 
-class WarRoomController extends BaseController {
+class WarRoomController extends BaseController
+{
 
     public function __construct()
     {
@@ -29,12 +30,12 @@ class WarRoomController extends BaseController {
 
         if (Cache::has($cacheKey)) {
             $content = Cache::get($cacheKey);
-        }else{
+        } else {
 
             // AO data
 
             $aos = AO::all();
-            foreach($aos as &$ao){
+            foreach ($aos as &$ao) {
                 $ao->couchData = $couchAPI->getMapTotals($ao->configName);
             }
 
@@ -44,7 +45,7 @@ class WarRoomController extends BaseController {
 
             $devs = Profile::where('remark', '=', 'Developer')->get();
 
-            forEach($devs as &$dev) {
+            forEach ($devs as &$dev) {
 
                 $dev->couchData = $couchAPI->getDevCredits($dev->a3_id);
 
@@ -59,11 +60,11 @@ class WarRoomController extends BaseController {
                 $size = '';
                 $sizeicon = '';
 
-                if(count($orbattype) > 0){
+                if (count($orbattype) > 0) {
                     $icon = $orbattype[0]->icon;
                     $name = $orbattype[0]->name;
                 }
-                if(count($orbatsize) > 0){
+                if (count($orbatsize) > 0) {
                     $size = $orbatsize[0]->name;
                     $sizeicon = $orbatsize[0]->icon;
                 }
@@ -81,7 +82,7 @@ class WarRoomController extends BaseController {
 
             $clans = Clan::where('parent', '!=', 'JTF')->orwhereNull('parent')->get();
 
-            forEach($clans as &$clan) {
+            forEach ($clans as &$clan) {
 
                 $clan->couchData = $couchAPI->getGroupTotalsByTag($clan->tag);
 
@@ -96,22 +97,22 @@ class WarRoomController extends BaseController {
                 $lat = '';
                 $lon = '';
 
-                if(count($orbattype) > 0){
+                if (count($orbattype) > 0) {
                     $icon = $orbattype[0]->icon;
                     $name = $orbattype[0]->name;
                 }
-                if(count($orbatsize) > 0){
+                if (count($orbatsize) > 0) {
                     $size = $orbatsize[0]->name;
                     $sizeicon = $orbatsize[0]->icon;
                 }
 
-                if (is_null ($clan->lat)) {
-                    $lat = rand(3000,4500);
+                if (is_null($clan->lat)) {
+                    $lat = rand(3000, 4500);
                 } else {
                     $lat = $clan->lat;
                 }
-                if (is_null ($clan->lon)) {
-                    $lon = rand(1800,6400);
+                if (is_null($clan->lon)) {
+                    $lon = rand(1800, 6400);
                 } else {
                     $lon = $clan->lon;
                 }
@@ -164,42 +165,42 @@ class WarRoomController extends BaseController {
         try {
             $profile = Profile::where('a3_id', '=', $armaid)->first();
 
-			$data['profile'] = $profile;
-			$data['player_id'] = $armaid;
-			
-			if(!is_null($profile)){
-				$user = $profile->user;
-				$data['user'] = $user;
-				$clan = $profile->clan;
-				$data['clan'] = $clan;
-			}
+            $data['profile'] = $profile;
+            $data['player_id'] = $armaid;
 
-			// Get data that won't be displayed as table data
+            if (!is_null($profile)) {
+                $user = $profile->user;
+                $data['user'] = $user;
+                $clan = $profile->clan;
+                $data['clan'] = $clan;
+            }
+
+            // Get data that won't be displayed as table data
             $couchAPI = new Alive\CouchAPI();
             $playerTotals = $couchAPI->getPlayerTotals($armaid);
-			$playerDetails = $couchAPI->getPlayerDetails($armaid);
-			$playerWeapon = $couchAPI->getPlayerWeapon($armaid,false);
-			$playerVehicle = $couchAPI->getPlayerVehicle($armaid,false);
-			$playerClass = $couchAPI->getPlayerClass($armaid,false);
-			//$playerLoadout = $couchAPI->getPlayerLoadout($armaid, $clan->tag, $lastMissionName);
+            $playerDetails = $couchAPI->getPlayerDetails($armaid);
+            $playerWeapon = $couchAPI->getPlayerWeapon($armaid, false);
+            $playerVehicle = $couchAPI->getPlayerVehicle($armaid, false);
+            $playerClass = $couchAPI->getPlayerClass($armaid, false);
+            //$playerLoadout = $couchAPI->getPlayerLoadout($armaid, $clan->tag, $lastMissionName);
 
-			$playerdata = array(
-				"Totals" => $playerTotals,
-				"Details" => $playerDetails,
-				"Weapon" => $playerWeapon,
-				"Vehicle" => $playerVehicle,
-				"Class" => $playerClass
-			//	"Loadout" => $playerLoadout
-			);
-			$data['playerdata'] = $playerdata;
+            $playerdata = array(
+                "Totals" => $playerTotals,
+                "Details" => $playerDetails,
+                "Weapon" => $playerWeapon,
+                "Vehicle" => $playerVehicle,
+                "Class" => $playerClass
+                //	"Loadout" => $playerLoadout
+            );
+            $data['playerdata'] = $playerdata;
 
-			// Option to grab player data here, or do it in datatable within View
-			
-			// Get player totals data from couchdb to send to page
-			
-			// Get player superlatives
-			
-			// Get player alias, class, favourite weapons, vehicles,
+            // Option to grab player data here, or do it in datatable within View
+
+            // Get player totals data from couchdb to send to page
+
+            // Get player superlatives
+
+            // Get player alias, class, favourite weapons, vehicles,
 
             return View::make('warroom/personnel.show')->with($data);
 
@@ -225,18 +226,18 @@ class WarRoomController extends BaseController {
      *
      * @return mixed
      */
-	public function getShowoperation()
+    public function getShowoperation()
     {
-		$name = Input::get('name');
-		$map = Input::get('map');
-		$clan = Input::get('clan');
-		
+        $name = Input::get('name');
+        $map = Input::get('map');
+        $clan = Input::get('clan');
+
         $data = get_default_data();
-		
-		// Get Map and Clan
-		$data['name'] = urldecode($name);
+
+        // Get Map and Clan
+        $data['name'] = urldecode($name);
         $data['ao'] = AO::where('configName', '=', urldecode($map))->first();
-		$data['clan'] = Clan::where('tag', '=', urldecode($clan))->first();
+        $data['clan'] = Clan::where('tag', '=', urldecode($clan))->first();
 
         return View::make('warroom/operations.show')->with($data);
     }
@@ -249,20 +250,20 @@ class WarRoomController extends BaseController {
     public function getOrbat()
     {
         $data = get_default_data();
-		
-		// Create a list of server IP addresses and clan names to view in datatables
-		$clans = Clan::all();
-		$clansArr = array();
-		foreach($clans as $clan) {
 
-			$clanServer = array();
-			$clanServer["tag"] = $clan->tag;
-			$clanServer["Name"] = $clan->name;
-			$clanServer["id"] = $clan->id;
-			array_push($clansArr,$clanServer);
-		}
-		
-		$data['clansArr'] = json_encode($clansArr); 
+        // Create a list of server IP addresses and clan names to view in datatables
+        $clans = Clan::all();
+        $clansArr = array();
+        foreach ($clans as $clan) {
+
+            $clanServer = array();
+            $clanServer["tag"] = $clan->tag;
+            $clanServer["Name"] = $clan->name;
+            $clanServer["id"] = $clan->id;
+            array_push($clansArr, $clanServer);
+        }
+
+        $data['clansArr'] = json_encode($clansArr);
 
         return View::make('warroom/orbat.index')->with($data);
     }
@@ -282,36 +283,40 @@ class WarRoomController extends BaseController {
         try {
             $clan = Clan::findOrFail($id);
             $data['clan'] = $clan;
-			
-			// Get group stats from couch
-			$couchAPI = new Alive\CouchAPI();
-			$clanTotals = $couchAPI->getGroupTotalsByTag($clan->tag);	
-			$clanLastOp = $couchAPI->getGroupLastOp($clan->tag);
-			$data['clanTotals'] = $clanTotals;
-			$data['clanLastOp']	= $clanLastOp;	
-				
-			$soldiers = array();
-			$officers = array();
-			
-			$members = $clan->members->all();
-			
-			forEach($members as $member) {
-					$tuser = Sentry::findUserById($member->user_id);
-					$memberIsOfficer = $tuser->inGroup($auth['officerGroup']);
-					$memberIsLeader = $tuser->inGroup($auth['leaderGroup']);
 
-					if($memberIsLeader) $leader = $member;
-					
-					if($memberIsOfficer) array_push($officers,$member);
+            // Get group stats from couch
+            $couchAPI = new Alive\CouchAPI();
+            $clanTotals = $couchAPI->getGroupTotalsByTag($clan->tag);
+            $clanLastOp = $couchAPI->getGroupLastOp($clan->tag);
+            $data['clanTotals'] = $clanTotals;
+            $data['clanLastOp'] = $clanLastOp;
 
-			}
-				
-			$data['leader'] = $leader;
-			$data['officers'] = $officers;
-			$data['soldiers'] = $members;
+            $soldiers = array();
+            $officers = array();
 
-			$data['clanOrbat'] = $clan->orbat();
-			
+            $members = $clan->members->all();
+
+            forEach ($members as $member) {
+                $tuser = Sentry::findUserById($member->user_id);
+                $memberIsOfficer = $tuser->inGroup($auth['officerGroup']);
+                $memberIsLeader = $tuser->inGroup($auth['leaderGroup']);
+
+                if ($memberIsLeader) {
+                    $leader = $member;
+                }
+
+                if ($memberIsOfficer) {
+                    array_push($officers, $member);
+                }
+
+            }
+
+            $data['leader'] = $leader;
+            $data['officers'] = $officers;
+            $data['soldiers'] = $members;
+
+            $data['clanOrbat'] = $clan->orbat();
+
             return View::make('warroom/orbat.show')->with($data);
 
         } catch (ModelNotFoundException $e) {
